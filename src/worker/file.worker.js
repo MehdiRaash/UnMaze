a4f00eaf116e5e7fcfb284b38dc0be63928a7fbd
mehdiRaash <leaksin@gmail.com>
2019-10-07 21:04:27 +0330

    better gaming experience without special perisan char

--- src/worker/file.worker.js
+++ src/worker/file.worker.js
@@ -1,8 +1,147 @@
-import words from '../files/Persian_words.json';
+//import words from '../files/Persian_words.json';
+const log = console.log;
 
 onmessage = function (e) {
   //console.log(e.data)
 };
-let y = {"dd":"dd"}
-postMessage(y);
- 
+
+//postMessage(y);
+
+const persianAlphabetMap = () => {
+  let fa = new Map();
+  fa.set(0, { char: 'ا' });
+  fa.set(1, { char: 'ب' });
+  fa.set(2, { char: 'پ' });
+  fa.set(3, { char: 'ت' });
+  fa.set(4, { char: 'ث' });
+  fa.set(5, { char: 'ج' });
+  fa.set(6, { char: 'چ' });
+  fa.set(7, { char: 'ح' });
+  fa.set(8, { char: 'خ' });
+  fa.set(9, { char: 'د' });
+  fa.set(10, { char: 'ذ' });
+  fa.set(11, { char: 'ر' });
+  fa.set(12, { char: 'ز' });
+  fa.set(13, { char: 'ژ' });
+  fa.set(14, { char: 'س' });
+  fa.set(15, { char: 'ش' });
+  fa.set(16, { char: 'ص' });
+  fa.set(17, { char: 'ض' });
+  fa.set(18, { char: 'ط' });
+  fa.set(19, { char: 'ظ' });
+  fa.set(20, { char: 'ع' });
+  fa.set(21, { char: 'غ' });
+  fa.set(22, { char: 'ف' });
+  fa.set(23, { char: 'ق' });
+  fa.set(24, { char: 'ک' });
+  fa.set(25, { char: 'گ' });
+  fa.set(26, { char: 'ل' });
+  fa.set(27, { char: 'م' });
+  fa.set(28, { char: 'ن' });
+  fa.set(29, { char: 'و' });
+  fa.set(30, { char: 'ه' });
+  fa.set(31, { char: 'ی' });
+  fa.set(32, { char: 'ء' });
+  fa.set(33, { char: 'آ' });
+  fa.set(34, { char: 'اً' });
+  fa.set(35, { char: 'ة' });
+  fa.set(36, { char: 'هٔ' });
+  return fa;
+}
+const fa = [
+  'ا',
+  'ب',
+  'پ',
+  'ت',
+  'ث',
+  'ج',
+  'چ',
+  'ح',
+  'خ',
+  'د',
+  'ذ',
+  'ر',
+  'ز',
+  'ژ',
+  'س',
+  'ش',
+  'ص',
+  'ض',
+  'ط',
+  'ظ',
+  'ع',
+  'غ',
+  'ف',
+  'ق',
+  'ک',
+  'گ',
+  'ل',
+  'م',
+  'ن',
+  'و',
+  'ه',
+  'ی',
+  'ء',
+  'آ',
+  'اً',
+  'ة',
+  'هٔ']; 
+
+
+
+
+let words = { "آب": 1, "درمان": 1, "آب": 1, "آبادان": 1 };
+words = { "آبی": '1' };
+
+class Node {
+  constructor(char, isLeaf = false) {
+    if (char === null) {
+      this.type = 'root';
+    }
+    this.char = char;
+    this.isLeaf = isLeaf;
+    this.children = new Map();
+
+  }
+  addChild(node) {
+    let indexOf = fa.indexOf(node.char);
+    if(indexOf !== -1){
+      this.children.set(indexOf, node)
+    }else{
+      console.warn(node, 'a node char has not found!!!')
+    }
+  }
+  setAsLeaf(){
+    this.isLeaf = true;
+  }
+}
+
+let rootNode = new Node(null);
+let newWord;
+
+let parentNode = rootNode;
+/** iterate over each word and just build the graph of Trie */
+for (word in words) {
+  newWord = true;
+  /** make sure to have an array representaition of word */
+  let wordInArr = word.split('');
+  //log(wordInArr)
+
+  wordInArr.forEach((char, index) => {
+
+    let node = new Node(char);
+    parentNode.addChild(node); 
+
+    /** if this is the last Char, make the node as a leaf node*/
+    if (index === wordInArr.length - 1) {
+      node.setAsLeaf();
+      parentNode = node;
+    } else {
+      parentNode = node;
+    }
+
+  });
+
+  
+}
+log(rootNode)
