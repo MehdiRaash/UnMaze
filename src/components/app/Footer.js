08f54068b92c6d01f223bbb52ba8b7172ae4970e
mehdiRaash <leaksin@gmail.com>
2019-10-15 01:18:33 +0330

    Update Footer.js

--- src/components/app/Footer.js
+++ src/components/app/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 
 function footer() {
   return (
-    <div className="text-center"></div>
+    <div className="text-center" >Made by Mehdi Raash, <a href="https://github.com/MehdiRaash/unmaze" style={{color:'darkred'}}>Github repo</a></div>
   );
 }
 
